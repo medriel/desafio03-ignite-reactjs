19fb5ff28e0aeb63d6bb73019c00bdb0b3ed5b08
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-08-04 19:36:14 -0300

    Implementing layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +27,53 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Algum titulo</strong>
+              <p>Pensando em sincronização em vez de cliclos de vida.</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  04 Ago 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Adriel Medeiros
+                </li>
+              </ul>
+            </a>
+          </Link>
+
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Algum titulo</strong>
+              <p>Pensando em sincronização em vez de cliclos de vida.</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  04 Ago 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Adriel Medeiros
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <button type="button">
+            Carregar mais posts
+          </button>
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
